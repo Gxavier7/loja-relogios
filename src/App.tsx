c9f169cab55a626190e3ef975be4c9f88c952878
Rafael N <rafaelngoncalves5@outlook.com>
2022-12-27 14:33:47 -0300

    React router dom configurado!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Test from './Componentes/Test';
 
 function App() {
   return (
     <div className="App">
-      
+      { /* Coloque suas rotas aqui! */ }
+      <Router>
+        <Routes>
+          <Route path='/test' element={ <Test></Test> }></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Componentes/Test.tsx
@@ -0,0 +1,4 @@
+export default function Test()
+{
+    return <h1 className="teal p-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ea harum modi quidem eos provident? Voluptas impedit labore accusamus cumque et delectus ipsam, consequatur magnam recusandae nisi ut quas amet eligendi!</h1>
+}
